a969dacc640a64eb6563a9a94518731404af03a9
Johannes Pries <johpries05@gmail.com>
2024-04-02 20:43:10 +0200

    add minus svg on click

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 
 let paragraph = document.querySelectorAll(".content > p");
 let button = document.querySelectorAll(".content > a");
+let img = document.querySelectorAll(".content>a>img");
 
 
 for (let i=0; i<button.length; i++) {
@@ -17,9 +18,11 @@ for (let i=0; i<button.length; i++) {
         if (paragraph[i].style.display === "none")
         {
             paragraph[i].style.display = "block";
+            img[i].src = "./assets/images/icon-minus.svg";
         } 
         else {
             paragraph[i].style.display = "none";
+            img[i].src = "./assets/images/icon-plus.svg";
         }
     });
 }
