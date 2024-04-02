c0716972381e6e79e66c9690e10e1df5d6d639b5
Johannes Pries <johpries05@gmail.com>
2024-04-02 20:36:59 +0200

    add Javascript for card funktion

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+
+let paragraph = document.querySelectorAll(".content > p");
+let button = document.querySelectorAll(".content > a");
+
+
+for (let i=0; i<button.length; i++) {
+    button[i].addEventListener("click", (e) => {
+        for (let j=0; j<paragraph.length; j++){
+            if (j!==i)
+            {
+                paragraph[j].style.display = "none";
+            }
+        }
+
+        if (paragraph[i].style.display === "none")
+        {
+            paragraph[i].style.display = "block";
+        } 
+        else {
+            paragraph[i].style.display = "none";
+        }
+    });
+}
